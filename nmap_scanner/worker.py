d4efd526d37aca95b94e41646baed862160a2342
Milan Bohacek <244656@muni.cz>
2025-07-29 22:21:24 +0200

    create testing workflow starter in nmap worker

--- nmap_scanner/worker.py
+++ nmap_scanner/worker.py
@@ -1,11 +1,38 @@
 import asyncio
 
-from easyeasm_demo.config import AppConfig
-from easyeasm_demo.workflow import EasyEasmWorkflow
+from temporalio.common import WorkflowIDReusePolicy
+from temporalio.exceptions import WorkflowAlreadyStartedError
+
+from easyeasm_demo.config import AppConfig, TemporalConfig
+from easyeasm_demo.workflow import EasyEasmWorkflow, logger
 from nmap_scanner.workflow import NmapWorkflow
 from temporalio.client import Client
 from temporalio.worker import Worker
 from temporalio.worker.workflow_sandbox import SandboxedWorkflowRunner, SandboxRestrictions
+
+
+async def start_unique_workflow(workflow, workflow_id: str, client: Client) -> None:
+
+    try:
+        # noinspection PyTypeChecker
+        workflow_handle = await client.start_workflow(
+            workflow.run,
+            args=(),
+            id=workflow_id,
+            task_queue="nmap_scanner_task_queue",
+            id_reuse_policy=WorkflowIDReusePolicy.ALLOW_DUPLICATE,
+        )
+        workflow_description = await workflow_handle.describe()
+        logger.info(
+            "Workflow start requested.", workflow_id=workflow_description.id, run_id=workflow_description.run_id
+        )
+    except WorkflowAlreadyStartedError as ex:
+        workflow_id_ex: str = str(ex.workflow_id)  # pyright: ignore
+        logger.warning(
+            "Workflow start already requested, doing nothing. "
+            "This is normal for multiple workers running concurrently.",
+            workflow_id=workflow_id_ex,
+        )
 
 
 async def main() -> None:
@@ -17,17 +44,19 @@ async def main() -> None:
         activities += workflow.get_activities()
     workflow_runner = SandboxedWorkflowRunner(
         restrictions=SandboxRestrictions.default.with_passthrough_modules(
-            "nmap_scan",
+            "nmap_scanner",
         )
     )
 
     worker = Worker(
         client=client,
-        task_queue=config.temporal.task_queue,
+        task_queue="nmap_scanner_task_queue",
         workflows=workflows,
         activities=activities,
         workflow_runner=workflow_runner,
     )
+
+    await start_unique_workflow(NmapWorkflow, "1", client)
     await worker.run()
 
 
