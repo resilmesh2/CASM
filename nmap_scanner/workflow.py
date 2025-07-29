a9d052ab1559be7a71b770ee3d604da36fd750a3
Milan Bohacek <244656@muni.cz>
2025-07-29 22:20:52 +0200

    merge activities in workflow for testing

--- nmap_scanner/workflow.py
+++ nmap_scanner/workflow.py
@@ -1,25 +1,24 @@
 from collections.abc import Awaitable, Callable, Sequence
+from datetime import timedelta
 from typing import Any
 
 from easyeasm_demo.config import AppConfig
 from nmap_scanner.activities import NmapActivities
-from nmap_scanner.dtos import NmapResults
 from temporalio import workflow
 
 
-@workflow.defn
+@workflow.defn(name="NmapWorkflow")
 class NmapWorkflow:
     @workflow.run
     async def run(self) -> None:
-        nmap_results = await workflow.execute_activity(
-            NmapActivities.run_nmap_scan,
-            start_to_close_timeout=30
-        )
+        # nmap_results = await workflow.execute_activity(
+        #     NmapActivities.run_nmap_scan,
+        #     start_to_close_timeout=timedelta(minutes=5),
+        # )
 
         await workflow.execute_activity(
             NmapActivities.parse_nmap_xml,
-            nmap_results,
-            start_to_close_timeout=30
+            start_to_close_timeout=timedelta(minutes=5),
         )
 
     @classmethod
