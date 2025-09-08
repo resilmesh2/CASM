feea53d7d08c4d5d466d65db2fcd8880f63722e3
Milan Bohacek <244656@muni.cz>
2025-09-08 17:17:24 +0200

    update complete_workflow.py

--- temporal/enumeration/complete_workflow.py
+++ temporal/enumeration/complete_workflow.py
@@ -1,5 +1,11 @@
+import asyncio
+import uuid
 from collections.abc import Awaitable, Callable, Sequence
+from logging import getLogger
 from typing import Any
+
+from temporalio.client import Client
+from temporalio.common import WorkflowIDReusePolicy
 
 from config import AppConfig
 from temporal.enumeration.active_enumeration.activities import ActiveEnumerationActivities
@@ -13,20 +19,21 @@ from temporalio import workflow
 @workflow.defn
 class CompleteScanWorkflow:
     @workflow.run
-    async def run(self, domains: list[str], wordlist: str,) -> str:
-        passive_result = await workflow.execute_child_workflow(
+    async def run(self) -> str:
+        config = AppConfig.get()
+        passive_result_uuid = await workflow.execute_child_workflow(
             PassiveEnumerationWorkflow.run,
-            args=[domains],
+            args=[config.scanner.domains],
             id=f"passive-{workflow.info().workflow_id}",
-            task_queue="subdomain-task-queue",
+            task_queue=config.temporal.easyeasm_task_queue,
         )
 
         # 2) Active
         active_result: str = await workflow.execute_child_workflow(
             ActiveEnumeratonWorkflow.run,
-            args=[passive_result, wordlist],
+            args=[passive_result_uuid, config.scanner.wordlist],
             id=f"active-{workflow.info().workflow_id}",
-            task_queue="subdomain-task-queue",
+            task_queue=config.temporal.easyeasm_task_queue,
         )
 
         return active_result
@@ -34,7 +41,30 @@ class CompleteScanWorkflow:
     @classmethod
     def get_activities(cls) -> Sequence[Callable[..., Awaitable[Any]]]:
         config = AppConfig.get()
-        passive_enum_activities = PassiveEnumerationActivities(config.redis, config.scanner.domains)
+        passive_enum_activities = PassiveEnumerationActivities(config.redis)
         active_enum_activities = ActiveEnumerationActivities(config.redis)
         utility_activities = UtilityActivities()
-        return [*passive_enum_activities.get_activities(), *active_enum_activities.get_activities(), *utility_activities.get_unique_subdomains]
+        return [*passive_enum_activities.get_activities(), *active_enum_activities.get_activities(), utility_activities.get_unique_subdomains]
+
+
+async def main() -> None:
+    config = AppConfig.get()
+    client = await Client.connect(config.temporal.url)
+    logger = getLogger()
+    workflow_id = uuid.uuid4().hex
+    # noinspection PyTypeChecker
+    workflow_handle = await client.start_workflow(
+        CompleteScanWorkflow.run,
+        args=(),
+        id=workflow_id,
+        task_queue=config.temporal.easyeasm_task_queue,
+        id_reuse_policy=WorkflowIDReusePolicy.ALLOW_DUPLICATE,
+    )
+    workflow_description = await workflow_handle.describe()
+    logger.info(
+        "Workflow start requested.", workflow_id=workflow_description.id, run_id=workflow_description.run_id
+    )
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
