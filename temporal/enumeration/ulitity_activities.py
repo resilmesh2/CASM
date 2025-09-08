e0d82343c4a5afc4c98512e6fa9a67fde558bd8d
Milan Bohacek <244656@muni.cz>
2025-09-08 17:18:12 +0200

    update utility activities with redis

--- temporal/enumeration/ulitity_activities.py
+++ temporal/enumeration/ulitity_activities.py
@@ -1,5 +1,6 @@
 import csv
 import io
+import uuid
 
 from redis import Redis
 
@@ -8,12 +9,21 @@ from temporalio import activity
 
 
 class UtilityActivities:
+    def __init__(self, redis_config: RedisConfig) -> None:
+        self.redis_config = redis_config
+
     @activity.defn
-    async def get_unique_subdomains(self, *data: str) -> set:
+    async def get_unique_subdomains(self, *data_redis_uuids: str) -> str:
         unique_subdomains = set()
-        for item in data:
-            unique_subdomains.update(item.splitlines())
-        return unique_subdomains
+        redis_client = Redis(host=self.redis_config.host, port=self.redis_config.port, db=0)
+        for item in data_redis_uuids:
+            unique_subdomains.update(redis_client.get(item).splitlines())
+        str_unique_subdomains = "\n".join(unique_subdomains)
+
+        unique_subdomains_uuid = f"unique_subdomains-{str(uuid.uuid4())}"
+        redis_client.set(unique_subdomains_uuid, str_unique_subdomains)
+        redis_client.close()
+        return unique_subdomains_uuid
 
     @activity.defn
     async def csv_to_txt_unique_hosts(self, redis_config: RedisConfig, scan_uuid: str) -> str:
@@ -26,13 +36,12 @@ class UtilityActivities:
         # Get CSV data from Redis
         csv_data = redis_client.get(f"passive_output_csv:{scan_uuid}")
         if not csv_data:
-            print(f"No CSV data found in Redis for scan_uuid: {scan_uuid}")
             redis_client.close()
             return ""
 
         # Parse CSV data
         seen = set()
-        reader = csv.DictReader(io.StringIO(csv_data.decode('utf-8')))
+        reader = csv.DictReader(io.StringIO(csv_data.decode("utf-8")))
         for row in reader:
             host = row.get("host", "").strip()
             if host:
