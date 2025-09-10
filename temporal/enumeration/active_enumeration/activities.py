77daa7cd7058e0c4fb446249c75caeaea5c884fc
Milan Bohacek <244656@muni.cz>
2025-09-10 17:20:12 +0200

    update alterx activity name

--- temporal/enumeration/active_enumeration/activities.py
+++ temporal/enumeration/active_enumeration/activities.py
@@ -15,12 +15,12 @@ class ActiveEnumerationActivities:
         return await activities_impl.active_dnsx(passive_scan_domains_uuid, wordlist, self.redis_config)
 
     @activity.defn
-    async def active_alterx(self, dnsx_output_uuid: str) -> str:
-        return await activities_impl.active_alterx(dnsx_output_uuid, self.redis_config)
+    async def active_alterx_with_dnsx(self, dnsx_output_uuid: str) -> str:
+        return await activities_impl.active_alterx_with_dnsx(dnsx_output_uuid, self.redis_config)
 
     @activity.defn
     async def active_httpx(self, alterx_domains_uuid: str) -> str:
         return await activities_impl.active_httpx(alterx_domains_uuid, self.redis_config)
 
     def get_activities(self) -> Sequence[Callable[..., Awaitable[Any]]]:
-        return [self.active_dnsx, self.active_httpx, self.active_alterx]
+        return [self.active_dnsx, self.active_httpx, self.active_alterx_with_dnsx]
