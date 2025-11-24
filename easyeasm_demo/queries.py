ce36e85693aa93fccaa02f5c9cb39a1dd6cc9d98
Sadlek <445581@mail.muni.cz>
2025-11-24 17:58:35 +0100

    Update Nmap endpoint and EasyEASM query.

--- easyeasm_demo/queries.py
+++ easyeasm_demo/queries.py
@@ -2,14 +2,14 @@ CASM_INSERT_QUERY = """
 WITH apoc.convert.fromJsonMap($json_string) AS input_, datetime.truncate('second', datetime.fromepochmillis(TIMESTAMP())) as scan_dt
         UNWIND input_.data AS row
         MERGE (ipadd:IP { address: row.ip })
-            ON CREATE SET ipadd.tag = ["unknown"]
+            ON CREATE SET ipadd.status = "unknown"
             SET ipadd.tag = apoc.coll.toSet(ipadd.tag + ["CASM"])
         MERGE (node:Node)-[r1:HAS_ASSIGNED]->(ipadd)
             ON CREATE SET r1.start = scan_dt
         MERGE (host:Host)<-[:IS_A]-(node)
         WITH host, row, ipadd, scan_dt
         MERGE (dn: DomainName { domain_name: row.domain_name})
-            ON CREATE SET dn.tag = ["unknown"]
+            ON CREATE SET dn.status = "unknown"
             SET dn.tag = apoc.coll.toSet(["A/AAAA", "CASM"] + dn.tag)
         WITH host, row, dn, ipadd, scan_dt
         OPTIONAL MATCH (dn)<-[r2:RESOLVES_TO]-(ipadd) WHERE r2.end IS NULL
@@ -26,7 +26,7 @@ WITH apoc.convert.fromJsonMap($json_string) AS input_, datetime.truncate('second
         OPTIONAL MATCH (ns)<-[r3:ON]-(host) WHERE r3.end IS NULL
             FOREACH(r IN CASE WHEN r3 IS NULL THEN [r3] ELSE [] END |
                 MERGE (ns)<-[ns_h:ON { start:  scan_dt}]-(host)
-                ON CREATE SET ns_h.tag = ["unknown", "CASM"]
+                ON CREATE SET ns_h.tag = ["CASM"], ns_h.status = "unknown" 
             )
         WITH host, row, scan_dt
         UNWIND row.software_versions AS software_version
